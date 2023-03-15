877cc47e1fd1c4e8af091a1b05800736948960d9
Yenhj <yen.greact@gmail.com>
2023-03-15 19:59:04 +0900

    장바구니 하는중... 집가서해야징

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -1,9 +1,14 @@
 import Button from "../button/Button";
+import CartItem from "../cart-item/CartItem";
 import "./cartDropdown.scss";
+
 const CartDropdown = () => {
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-items"></div>
+      <div className="cart-items">
+        {}
+        <CartItem />
+      </div>
       <Button>GO TO CHECKOUT</Button>
     </div>
   );
--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -1,12 +1,25 @@
 import { createContext, useState } from "react";
 
+const addCartItem = (cartItems, productToAdd) => {
+  //지금 장바구니에 있는지없는지
+  //있으면 수량 증가
+  //없으면 새로 생성
+};
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
+  // 장바구니 클릭시 보이는 제품들
+  cartItems: [],
+  addItemToCart: () => {},
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  };
   const value = { isCartOpen, setIsCartOpen };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
